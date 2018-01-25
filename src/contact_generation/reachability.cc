04f72c5f2d8db32a1efc325a9f577f7da13c864a
Pierre Fernbach <pierre.fernbach@laas.fr>
2018-01-25 17:22:29 +0100

    [transition test] add method 'stack constraints'

--- src/contact_generation/reachability.cc
+++ src/contact_generation/reachability.cc
@@ -6,7 +6,14 @@ namespace hpp {
    namespace reachability{
 
 std::pair<MatrixXX, MatrixXX> stackConstraints(const std::pair<MatrixXX, MatrixXX>& Ab,const std::pair<MatrixXX, MatrixXX>& Cd){
-
+    size_t numIneq = Ab.first.rows() + Cd.first.rows();
+    MatrixXX M(numIneq,3);
+    VectorX  N(numIneq);
+    M.block(0,0,Ab.first.rows(),3) = Ab.first;
+    M.block(Ab.first.rows(),0,Cd.first.rows(),3) = Cd.first;
+    N.segment(0,Ab.first.rows()) = Ab.second;
+    N.segment(Ab.first.rows(),Cd.first.rows()) = Cd.second;
+    return std::make_pair(M,n);
 }
 
 
