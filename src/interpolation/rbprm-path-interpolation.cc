humanoid-path-planner/hpp-rbprm

commit 5a25035ff581c8c8f76e06aa8bc5288250e390b6

[contact-generation] Correctly take the previous valid state configuration as init for the projection in maintainContact (instead of the startState_)

src/interpolation/rbprm-path-interpolation.cc
@@ -138,7 +138,8 @@ namespace hpp {
         for(CIT_Configuration cit = configs.begin()+1; cit != configs.end(); ++cit, currentVal+= timeStep)
         {
             const State& previous = states.back().second;
-            core::Configuration_t configuration = *cit;
+            core::Configuration_t configuration = previous.configuration_;
+            configuration.head<7>() = (*cit).head<7>();
             acc = configuration.segment<3>(accIndex);
             //dir = configuration.head<3>() - previous.configuration_.head<3>();
             dir = configuration.segment<3>(accIndex-3);

commit 1436314caec6482eaf27a0e747dd3dff545e7b1c

[contact-generation] fix an error introduced in 5a25035 : correctly take the extraDof from planning in contact generation

src/interpolation/rbprm-path-interpolation.cc
@@ -113,6 +113,19 @@ namespace hpp {
         return results;
     }
 
+    /**
+     * @brief loadPreviousConfiguration take the root and extra dof config from config, and the whole body from previous
+     * @param previous take the whole body configuration of the configuration
+     * @param config take the root and extra dof configuration of this configuration
+     * @return
+     */
+    core::Configuration_t loadPreviousConfiguration(const DevicePtr_t& device,const core::Configuration_t& previous,const core::Configuration_t& config){
+        core::Configuration_t res(previous);
+        res.head<7>() = config.head<7>();
+        res.tail(device->extraConfigSpace().dimension()) = config.tail(device->extraConfigSpace().dimension()) ;
+        return res;
+    }
+
     rbprm::T_StateFrame RbPrmInterpolation::Interpolate(const affMap_t& affordances,
                                                         const std::map<std::string, std::vector<std::string> >& affFilters,
                                                         const hpp::rbprm::T_Configuration &configs, const double robustnessTreshold,
@@ -138,8 +151,7 @@ namespace hpp {
         for(CIT_Configuration cit = configs.begin()+1; cit != configs.end(); ++cit, currentVal+= timeStep)
         {
             const State& previous = states.back().second;
-            core::Configuration_t configuration = previous.configuration_;
-            configuration.head<7>() = (*cit).head<7>();
+            core::Configuration_t configuration = loadPreviousConfiguration(robot_->device_,previous.configuration_,*cit);
             acc = configuration.segment<3>(accIndex);
             //dir = configuration.head<3>() - previous.configuration_.head<3>();
             dir = configuration.segment<3>(accIndex-3);