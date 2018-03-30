a602a915a0c5399ff398bb005f73460740541fe7
Pierre Fernbach <pierre.fernbach@laas.fr>
2018-03-30 09:22:16 +0200

    [planner] fix an error in rbprm-steering-kinodynamic : correctly fail when no acceleration are possible

--- src/planner/rbprm-steering-kinodynamic.cc
+++ src/planner/rbprm-steering-kinodynamic.cc
@@ -69,7 +69,7 @@ namespace hpp{
         return core::PathPtr_t();
       hppStopBenchmark(FIND_A_MAX);
       hppDisplayBenchmark(FIND_A_MAX);
-      if(aMax_.norm() <= 0)
+      if((aMax_[0]+aMax_[1]) <= 0)
         return core::PathPtr_t();
       //return core::steeringMethod::Kinodynamic::impl_compute(*x->configuration(),q2);
       hppStartBenchmark(steering_kino);
@@ -185,7 +185,7 @@ namespace hpp{
         return core::PathPtr_t();
       hppStopBenchmark(FIND_A_MAX);
       hppDisplayBenchmark(FIND_A_MAX);
-      if(aMax_.norm() <= 0)
+      if((aMax_[0]+aMax_[1]) <= 0)
         return core::PathPtr_t();
       hppStartBenchmark(steering_kino);
       core::PathPtr_t path = core::steeringMethod::Kinodynamic::impl_compute(q1,*x->configuration());
