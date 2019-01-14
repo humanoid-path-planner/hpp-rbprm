1614f60a6647ee9270b40ce0628624ec1dce2b0c
stevet <stevetonneau@gotmail.fr>
2019-01-14 12:08:00 +0000

    rotation matrix simplification

--- src/tools.cc
+++ src/tools.cc
@@ -28,70 +28,10 @@ namespace hpp {
   namespace tools {
     Eigen::Matrix3d GetRotationMatrix(const Eigen::Vector3d &from, const Eigen::Vector3d &to)
     {
-        Eigen::Vector3d u, v, uXv;
-        Eigen::Vector3d  a;
-        u = from; u.normalize();
-        v = to  ; v.normalize();
-        uXv = u.cross(v);
-        Eigen::Matrix3d I = Eigen::Matrix3d::Identity();
-        double sinTheta = uXv.norm();
-        if (sinTheta < std::numeric_limits<double>::epsilon()) // angle is 0
-        {
-            return I;
-        }
-        else
-        {
-            double cosTheta = u.dot(v);
-            a = uXv / sinTheta;
-            Eigen::Matrix3d Iaaa;
-            Iaaa(0,1) = 0     ; Iaaa(0,1) = -a[2]; Iaaa(0,2) =  a[1]; //  0  -z   y
-            Iaaa(1,0) =  a[2] ; Iaaa(1,0) = 0    ; Iaaa(1,2) = -a[0]; //  z   0  -x
-            Iaaa(2,0) = -a[1] ; Iaaa(2,1) = a[0] ; Iaaa(2,2) =  0; // -y   x   0
-
-            return I * cosTheta + sinTheta * Iaaa + (1 - cosTheta) * (a*a.transpose());
-        }
+        Eigen::Quaterniond quat; quat.setFromTwoVectors(from, to);
+        return quat.toRotationMatrix();
     }
 
-    // TODO directly compute transformation, this is terrible
-    /*fcl::Matrix3f GetRotationMatrix(const fcl::Vec3f& from, const fcl::Vec3f& to)
-    {
-        fcl::Matrix3f result;
-        Eigen::Matrix3d resEigen = Eigen::Matrix3d::Identity();
-        Eigen::Vector3d u, v, uXv;
-        Eigen::Vector3d  a;
-        for(int i =0; i<3; ++i)
-        {
-            u[i] = from[i];
-            v[i] = to[i];
-        }
-        u.normalize();
-        v.normalize();
-        uXv = u.cross(v);
-        Eigen::Matrix3d I = Eigen::Matrix3d::Identity();
-        double sinTheta = uXv.norm();
-        if (sinTheta < std::numeric_limits<double>::epsilon()) // angle is 0
-        {
-            resEigen = I; // identity
-        }
-        else
-        {
-            double cosTheta = u.dot(v);
-            a = uXv / sinTheta;
-            Eigen::Matrix3d Iaaa;
-            Iaaa(0,1) = 0     ; Iaaa(0,1) = -a[2]; Iaaa(0,2) =  a[1]; //  0  -z   y
-            Iaaa(1,0) =  a[2] ; Iaaa(1,0) = 0    ; Iaaa(1,2) = -a[0]; //  z   0  -x
-            Iaaa(2,0) = -a[1] ; Iaaa(2,1) = a[0] ; Iaaa(2,2) =  0; // -y   x   0
-            resEigen = I * cosTheta + sinTheta * Iaaa + (1 - cosTheta) * (a*a.transpose());
-        }
-        for(int i = 0; i<3;++i)
-        {
-            for(int j = 0; j<3;++j)
-            {
-                result(i,j) = resEigen(i,j);
-            }
-        }
-        return result;
-    }*/
 
     fcl::Matrix3f GetZRotMatrix(const core::value_type theta)
     {
